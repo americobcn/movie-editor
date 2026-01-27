bad7a11283ebae54a6d54ed0e57077afc5636b05
americobcn <americo.cot@gmail.com>
2026-01-27 17:24:41 +0100

    From work

--- Movie Editor/Sources/AudioTapProcessor.swift
+++ Movie Editor/Sources/AudioTapProcessor.swift
@@ -4,7 +4,7 @@ import AVFoundation
 import Accelerate
 
 
-#if compiler(>=6.0) || swift(>=6.0)
+#if compiler(>=6.1) || swift(>=6.1)
 typealias AudioTapVersionType = MTAudioProcessingTap
 #else
 typealias AudioTapVersionType = Unmanaged<MTAudioProcessingTap>
@@ -72,7 +72,7 @@ final class AudioTapProcessor {
         let tap = try createAudioProcessingTap(processor: processor)
         let params = AVMutableAudioMixInputParameters(track: track)
 
-#if compiler(>=6.0) || swift(>=6.0)
+#if compiler(>=6.1) || swift(>=6.1)
         params.audioTapProcessor = tap
 #else
         params.audioTapProcessor = tap.takeUnretainedValue()
